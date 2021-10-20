62d83a5e7a775cf8cb1e362643bffd89a85927a2
Hojong <gim1039@naver.com>
2021-10-20 13:10:58 +0900

    first commit

---
+++ app/src/main/java/com/koreait/first/MenuActivity.java
@@ -0,0 +1,96 @@
+package com.koreait.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.koreait.first.ch07.BookPersonActivity;
+import com.koreait.first.picsum.PicsumActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void call(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW
+                , Uri.parse("tel:010-1111-2222"));
+        startActivity(intent);
+    }
+
+    public void moveToActivity(View v) {
+        int id = v.getId();
+
+        Class c = null;
+        if(id == R.id.menuBtn1) {
+            c = MainActivity.class;
+        } else if(id == R.id.menuBtn2) {
+            c = LinearActivity.class;
+        } else if(id == R.id.menuBtn3) {
+            c = ConstraintActivity.class;
+        } else if(id == R.id.menuBtn4) {
+            c = WriteActivity.class;
+        } else if(id == R.id.menuBtn5) {
+            c = BookPersonActivity.class;
+        } else if(id == R.id.menuBtn6) {
+            c = ImageViewActivity.class;
+        } else if(id == R.id.menuBtn7) {
+            c = PicsumActivity.class;
+        }
+
+        Intent intent = new Intent(this, c);
+        startActivity(intent);
+    }
+
+    public void moveToActivityWithText(View v) {
+        TextView tv = (TextView)v;
+        String text = (String)tv.getText();
+        Log.i("myLog", text);
+
+        Class c = null;
+        switch(text) {
+            case "메인":
+                c = MainActivity.class;
+                break;
+            case "리니어레이아웃":
+                c = LinearActivity.class;
+                break;
+            case "제약레이아웃":
+                c = ConstraintActivity.class;
+                break;
+        }
+
+        Intent intent = new Intent(this, c);
+        startActivity(intent);
+    }
+
+}
+
+        // 분기
+
+////    public void moveToMain(View v) {
+////        Class c = MainActivity.class;
+//        Intent intent = new Intent(this, c);
+//        startActivity(intent);
+//    }
+//}
+////    }
+////
+////    public void moveToLinear(View v) {
+////        Intent intent = new Intent(this, LinearActivity.class);
+////        startActivity(intent);
+////    }
+////
+////    public void moveToConstraint(View v) {
+////        Intent intent = new Intent(this, ConstraintActivity2.class);
+////        startActivity(intent);
+////    }
+////}
